609c5ea8babfd59ee52555ffc12faa91887c6927
milanstamenkovic <milanstamenkovic@live.com>
2016-06-08 20:32:27 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import edu.stanford.nlp.simple.*;
+
+import java.util.List;
+
+/**
+ * Created by Milan on 6/8/2016.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Sentence sent = new Sentence("Lucy is in the sky with diamonds.");
+        List<String> nerTags = sent.nerTags();  // [PERSON, O, O, O, O, O, O, O]
+        String firstPOSTag = sent.posTag(0);
+        // NNP
+
+        System.out.print(nerTags);
+    }
+}
