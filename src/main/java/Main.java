d35bc408c294059f2cec44415536d584b59cbb96
shomimn <shomimn@gmail.com>
2016-06-08 21:30:28 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,19 @@ import edu.stanford.nlp.simple.*;
 
 import java.util.List;
 
-/**
- * Created by Milan on 6/8/2016.
- */
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
-        Sentence sent = new Sentence("Lucy is in the sky with diamonds.");
-        List<String> nerTags = sent.nerTags();  // [PERSON, O, O, O, O, O, O, O]
-        String firstPOSTag = sent.posTag(0);    // NNP
-
-        System.out.print(nerTags);
+        Document doc = new Document("add your text here! It can contain multiple sentences.");
+        for (Sentence sent : doc.sentences()) {  // Will iterate over two sentences
+            // We're only asking for words -- no need to load any models yet
+            System.out.println("The second word of the sentence '" + sent + "' is " + sent.word(1));
+            // When we ask for the lemma, it will load and run the part of speech tagger
+            System.out.println("The third lemma of the sentence '" + sent + "' is " + sent.lemma(2));
+            // When we ask for the parse, it will load and run the parser
+            System.out.println("The parse of the sentence '" + sent + "' is " + sent.parse());
+            // ...
+        }
     }
 }
