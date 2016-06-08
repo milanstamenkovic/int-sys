79abe8de74634bcdfe4866d76a49ed01cf8990be
NemanjaT1992 <nemanjatomasevic71@gmail.com>
2016-06-08 20:43:31 +0200

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,7 @@ public class Main {
     {
         Sentence sent = new Sentence("Lucy is in the sky with diamonds.");
         List<String> nerTags = sent.nerTags();  // [PERSON, O, O, O, O, O, O, O]
-        String firstPOSTag = sent.posTag(0);
-        // NNP
+        String firstPOSTag = sent.posTag(0);    // NNP
 
         System.out.print(nerTags);
     }
